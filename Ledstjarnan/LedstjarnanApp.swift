e517db0cc9a43e8cd35599f8b8881c925bc95bb7
Murtaza-Majeed <murtaza.majeed@hotmail.com>
2026-01-20 12:26:57 +0100

    Initial Commit

---
+++ Ledstjarnan/LedstjarnanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedstjarnanApp.swift
+//  Ledstjarnan
+//
+//  Created by Murtaza Majeed on 2026-01-20.
+//
+
+import SwiftUI
+
+@main
+struct LedstjarnanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LedstjarnanTests/LedstjarnanTests.swift
@@ -0,0 +1,17 @@
+//
+//  LedstjarnanTests.swift
+//  LedstjarnanTests
+//
+//  Created by Murtaza Majeed on 2026-01-20.
+//
+
+import Testing
+@testable import Ledstjarnan
+
+struct LedstjarnanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
